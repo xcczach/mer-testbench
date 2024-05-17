32a19464a32dec7034770a61fb81c39eca21af73
xcc <xcc_zach@sjtu.edu.cn>
2024-05-17 10:19:56 +0800

    init test bench

--- test_bench.py
+++ test_bench.py
@@ -2,13 +2,13 @@ import pandas as pd
 import openai
 import ast
 import json
+import dotenv
 
-
+api_key = dotenv.get_key(".env", "API_KEY")
 client = openai.OpenAI(
-    api_key="",
+    api_key=api_key,
     base_url="https://api.xi-ai.cn/v1",
 )
-
 prompt = "请扮演情感领域的专家。我们将提供一组情绪。请将这些情绪分组，每组包含同义词或一致的情感术语。直接以Python列表形式输出结果。"
 
 
@@ -24,7 +24,6 @@ def get_emotion_ids(
     )
 
     arrs = ast.literal_eval(completion.choices[0].message.content)
-    # print(arrs)
     prediction_ids = set()
     true_ids = set()
     for emotion in prediction_emotions:
@@ -46,15 +45,11 @@ def get_recall(prediction_ids: set[int], true_ids: set[int]):
     return len(prediction_ids.intersection(true_ids)) / len(true_ids)
 
 
-if __name__ == "__main__":
-    prediction_emotion_strs = []
-    with open("result.json", "r", encoding="utf-8") as f:
-        prediction_json = json.load(f)
-    for item in prediction_json["data"]:
-        prediction_emotion_strs.append(item["emotion"])
-
-    df = pd.read_csv("final-openset-chinese.csv")
-    true_emotion_strs = list(df.iloc[:, 1].to_numpy())
+def test(
+    prediction_emotion_strs: list[str],
+    true_emotion_strs: list[str],
+    sample_ids: list[str] | None = None,
+) -> pd.DataFrame:
     result_df = pd.DataFrame(
         columns=[
             "sample_index",
@@ -65,6 +60,8 @@ if __name__ == "__main__":
             "score",
         ]
     )
+    if sample_ids is None:
+        result_df = result_df.drop(columns=["sample"])
 
     for i in range(len(prediction_emotion_strs)):
         try:
@@ -75,25 +72,30 @@ if __name__ == "__main__":
                 prediction_emotions, true_emotions
             )
             sample_index = i
-            sample = prediction_json["data"][i]["id"]
             accuracy = get_accuracy(prediction_ids, true_ids)
             recall = get_recall(prediction_ids, true_ids)
             score = (accuracy + recall) / 2
-            result_df.loc[i] = [
-                sample_index,
-                sample,
-                emotion_group,
-                accuracy,
-                recall,
-                score,
-            ]
+            result_df.loc[i] = (
+                [
+                    sample_index,
+                    sample_ids[i],
+                    emotion_group,
+                    accuracy,
+                    recall,
+                    score,
+                ]
+                if sample_ids is not None
+                else [
+                    sample_index,
+                    emotion_group,
+                    accuracy,
+                    recall,
+                    score,
+                ]
+            )
         except:
             print(f"sample {i} failed")
             continue
         print(f"sample {i}: accuracy: {accuracy}, recall: {recall}, score: {score}")
 
-    result_df.to_csv("eval_result.csv", index=False)
-    # get average
-    print("Accuracy", result_df["accuracy"].mean())
-    print("Recall", result_df["recall"].mean())
-    print("Score", result_df["score"].mean())
+    return result_df
