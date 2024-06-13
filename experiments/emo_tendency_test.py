908ed043308cc2a3c03b8a04bd45d3188d474a40
xcc <xcc_zach@sjtu.edu.cn>
2024-06-13 21:59:26 +0800

    bug fix

---
+++ experiments/emo_tendency_test.py
@@ -0,0 +1,4 @@
+# 读取数据
+# 读取/生成ground truth情感倾向
+# 读取/生成prediction情感倾向
+# 列出情感倾向不匹配的sample
--- official_custom_test.py
+++ official_custom_test.py
@@ -1,4 +1,4 @@
-# usage: python official_custom_test.py <prediction_file_name_without_csv>
+# usage: python official_custom_test.py <prediction_file_name_without_csv> <target_folder>
 import test_bench
 import pandas as pd
 import asyncio
@@ -8,7 +8,7 @@ prediction_df = pd.read_csv(f"{sys.argv[1]}.csv")
 prediction_emotion_strs = list(prediction_df.iloc[:, 1].to_numpy())
 sample_ids = list(prediction_df.iloc[:, 0].to_numpy())
 
-df = pd.read_csv("final-openset-chinese.csv")
+df = pd.read_csv("check-openset.csv")
 true_emotion_strs = list(df.iloc[:, 1].to_numpy())
 
 
@@ -35,7 +35,9 @@ async def main():
         result_str = f"round: {index}, accuracy: {item[1][0]}, recall: {item[1][1]}, score: {item[1][2]}"
         results_str += result_str + "\n"
         print(result_str)
-        item[0].to_csv(f"eval_result_{sys.argv[1]}_r{index}.csv", index=False)
+        item[0].to_csv(
+            f"{sys.argv[2]}/eval_result_{sys.argv[1]}_r{index}.csv", index=False
+        )
     accuracys, recalls, scores = zip(*[item[1] for item in results])
     confidence = 0.95
     mean_accuracy, margin_of_error_accuracy = test_bench.get_mean_and_error(
@@ -50,7 +52,7 @@ async def main():
     mean_result_str = f"accurary: {mean_accuracy} +- {margin_of_error_accuracy}, recall: {mean_recall} +- {margin_of_error_recall}, score: {mean_score} +- {margin_of_error_score}, confidence: {confidence}"
     print(mean_result_str)
     results_str += mean_result_str + "\n"
-    with open(f"eval_results_{sys.argv[1]}.txt", "w") as f:
+    with open(f"{sys.argv[2]}/eval_results_{sys.argv[1]}.txt", "w") as f:
         f.write(results_str)
 
 
